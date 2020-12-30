5570974cf29b61e4d1874653deca01fad35e1406
Deepak Kumar <deepak787098@gmail.com>
2020-12-30 14:57:15 +0530

    no msg

--- src/App.js
+++ src/App.js
@@ -1,43 +1,42 @@
-import React, { Component } from 'react';
-import classes from './App.module.css';
-import Card from './Card/Card';
-import loader from './assets/preloader.gif';
+import React, { Component } from "react";
+import classes from "./App.module.css";
+import Card from "./Card/Card";
+import loader from "./assets/preloader.gif";
 
-class App  extends Component {
+class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       movieData: [],
-      loading: true,
-     }
+      loading: true
+    };
   }
 
-  componentDidMount(){
-    fetch('https://www.omdbapi.com/?apikey=45f0782a&s=war')
-    .then(response => response.json())
-    .then(json => this.setState({movieData: json.Search, loading: false}))
+  componentDidMount() {
+    fetch("https://www.omdbapi.com/?apikey=45f0782a&s=war")
+      .then((response) => response.json())
+      .then((json) =>
+        this.setState({ movieData: json.Search, loading: false })
+      );
   }
 
-  render() { 
-    return ( 
+  render() {
+    return (
       <div className={classes.movie_page}>
-        {
-          this.state.loading ?
-            <img className={classes.loader_icon} alt="loder icon" src={loader} /> 
-            : 
-            <h3 className={classes.title}>Movies</h3>
-        }
-        
+        {this.state.loading ? (
+          <img className={classes.loader_icon} alt="loder icon" src={loader} />
+        ) : (
+          <h3 className={classes.title}>Movies</h3>
+        )}
+
         <div className={classes.cards_wrapper}>
-          {
-            this.state.movieData.map((data, index) => 
-              <Card data={data} key={index} />
-            )
-          }
+          {this.state.movieData.map((data, index) => (
+            <Card data={data} key={index} />
+          ))}
         </div>
       </div>
-     );
+    );
   }
 }
- 
+
 export default App;
